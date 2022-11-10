b8198763df3dd67fbc8d45f016f8d3a0b4c41fb7
Mehrdad Javan <mehrdad.javan@adapta.se>
2022-11-10 13:54:37 +0100

    option 1 - abstract class

---
+++ src/main/java/se/lexicon/model/Fruit.java
@@ -0,0 +1,23 @@
+package se.lexicon.model;
+
+public class Fruit extends Product {
+
+  private final String color;
+
+  public Fruit(int id, String fruitType, String color, double price) {
+    super(id, price, fruitType);
+    this.color = color;
+  }
+
+  @Override
+  public String examine() {
+    return "{ id: " + getId() + ", fruit: " + color + " " + getProductName() + " }";
+  }
+
+  @Override
+  public String use() {
+    return getProductName() + " taste delicious!";
+  }
+
+
+}
---
+++ src/main/java/se/lexicon/model/Product.java
@@ -0,0 +1,40 @@
+package se.lexicon.model;
+
+public abstract class Product {
+
+  private final int id;
+  private double price;
+  private String productName;
+
+  public Product(int id, double price, String productName) {
+    this.id = id;
+    this.price = price;
+    this.productName = productName;
+  }
+
+  public abstract String examine();
+
+  public abstract String use();
+
+
+
+  public int getId() {
+    return id;
+  }
+
+  public double getPrice() {
+    return price;
+  }
+
+  public void setPrice(double price) {
+    this.price = price;
+  }
+
+  public String getProductName() {
+    return productName;
+  }
+
+  public void setProductName(String productName) {
+    this.productName = productName;
+  }
+}
